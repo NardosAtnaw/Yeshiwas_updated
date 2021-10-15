5bf556af2af6cb604e912d1d24f5acbcb60a654d
Nardos_Dumessa <nardidumessa@gmail.com>
2021-10-15 17:15:52 +0300

    almost done6

--- js/app.js
+++ js/app.js
@@ -1,3 +1,74 @@
+const linksContainer = document.querySelector(".links-container");
+const nav = document.querySelector(".nav");
+const links = document.querySelectorAll('.link')
+const logo = document.querySelector(".logo");
+const menu = document.querySelector(".menu");
+const sidenav = document.querySelector(".links-container");
+const close = document.querySelector(".close-btn");
+menu.addEventListener("click", () => {
+  sidenav.classList.add("show-nav");
+  links.forEach(link => {
+    link.style.color = '#10284e'
+})
+});
+close.addEventListener("click", () => {
+  sidenav.classList.remove("show-nav");
+});
+
+if(window.innerWidth > 1152){
+  console.log("scroll");
+  console.log(window.innerWidth);
+  window.addEventListener("scroll", () => {
+    const navheight = nav.getBoundingClientRect().height;
+    const scorllheight = window.pageYOffset;
+    const upperText = document.querySelector('.upper-text')
+    const upperImg = document.querySelector(".upper-img")
+    if (scorllheight > navheight) {
+      menu.innerHTML = `<img src="./Image/menu3.svg" class="ham" alt="" />`;
+
+      logo.innerHTML = `<a href="./index.html"><img src="./Image/logo.svg" alt="Yeshiwas Logo" /> </a>`;
+      nav.classList.add("fixed-nav");
+      links.forEach(link => {
+          link.style.color = '#10284e'
+      })
+      upperText.style.color = '#10284e'
+      upperImg.setAttribute('src',"./Image/call3.svg")
+
+    } else {
+      logo.innerHTML = `<a href="./index.html"><img src="./Image/logo1.svg" alt="Yeshiwas Logo" />  </a>`;
+      nav.classList.remove("fixed-nav");
+      menu.innerHTML = ` <img src="./Image/menu.svg" class="ham" alt="" />`;
+      links.forEach(link => {
+          link.style.color = 'white'
+      })
+      upperText.style.color = 'white'
+      upperImg.setAttribute('src',"./Image/call1.svg")
+    }
+  });
+}else{
+  window.addEventListener("scroll", () => {
+    const navheight = nav.getBoundingClientRect().height;
+    const scorllheight = window.pageYOffset;
+    const upperText = document.querySelector('.upper-text')
+    const upperImg = document.querySelector(".upper-img")
+    if (scorllheight > navheight) {
+      menu.innerHTML = `<img src="./Image/menu3.svg" class="ham" alt="" />`;
+
+      logo.innerHTML = `<a href="./index.html"><img src="./Image/logo.svg" alt="Yeshiwas Logo" /> </a>`;
+      nav.classList.add("fixed-nav");
+      upperText.style.color = '#10284e'
+      upperImg.setAttribute('src',"./Image/call3.svg")
+
+    } else {
+      logo.innerHTML = `<a href="./index.html"><img src="./Image/logo1.svg" alt="Yeshiwas Logo" />  </a>`;
+      nav.classList.remove("fixed-nav");
+      menu.innerHTML = ` <img src="./Image/menu.svg" class="ham" alt="" />`;
+      upperText.style.color = 'white'
+      upperImg.setAttribute('src',"./Image/call1.svg")
+    }
+  });
+}
+
 const client1 = document.querySelector('.client1')
 const client2 = document.querySelector('.client2')
 
@@ -17,25 +88,3 @@ client1.addEventListener('mouseleave', () =>{
 
 
 
-window.addEventListener('scroll', () => {
-  const nav = document.querySelector('.nav')
-  const navHeight = nav.getBoundingClientRect().height;
-  const scrollHeight = window.pageYOffset;
-  if(scrollHeight > navHeight){
-    
-    nav.classList.add('fixed')
-  }else{
-    nav.classList.remove('fixed')
-  }
-})
-
-const menu = document.querySelector('.menu')
-const sidenav = document.querySelector('.home-cont')
-const close = document.querySelector('.close-btn')
-menu.addEventListener('click', () =>{
-  sidenav.classList.add('show-nav')
-  console.log("clicked");
-})
-close.addEventListener('click',() =>{
-  sidenav.classList.remove('show-nav')
-})
